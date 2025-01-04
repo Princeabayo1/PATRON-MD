eeece8218521b1f9cfe8cdcba5b98b99fe397278
Princeabayo1 <princelyrices@gmail.com>
2025-01-04 01:25:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "palmpay"
 
-global.banknumber = "8133729715"
+global.banknumber = "7011116971"
 
-global.bankowner = "FADARE"
+global.bankowner = "YISHAU"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
